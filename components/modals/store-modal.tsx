fa860f4237928cc3b25a916416555b3f1b5876cb
hassanziya <60289732+hassanziya@users.noreply.github.com>
2023-07-10 20:00:07 +0530

    completed-store-switcher-in-navbar

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,6 +3,9 @@
 import * as z from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { Modal } from '@/components/ui/modal';
@@ -16,13 +19,15 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '../ui/button';
+import LoaderSpinner from '../ui/spinner';
 
 const formSchema = z.object({
-  name: z.string().min(1),
+  name: z.string().min(5, 'Store name must contain at least 5 charaters'),
 });
 
 export const StoreModal = () => {
   const { isOpen, onClose } = useStoreModal();
+  const [loading, setLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -32,8 +37,15 @@ export const StoreModal = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    //TODO: Create Store
-    console.log(values);
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/stores', values);
+      window.location.assign(`/${response.data.id}`);
+    } catch (error) {
+      toast.error('something went wrong');
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <Modal
@@ -53,17 +65,29 @@ export const StoreModal = () => {
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Input placeholder="Store-name" {...field} />
+                      <Input
+                        disabled={loading}
+                        placeholder="Store-name"
+                        {...field}
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
               <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                <Button variant="outline" onClick={onClose}>
+                <Button
+                  type="button"
+                  variant="outline"
+                  onClick={onClose}
+                  disabled={loading}
+                >
                   Cancel
                 </Button>
-                <Button type="submit">Continue</Button>
+                <Button type="submit" disabled={loading}>
+                  {loading && <LoaderSpinner width={4} height={4} />}
+                  Continue
+                </Button>
               </div>
             </form>
           </Form>
